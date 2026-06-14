808427453576fb2c4798dae62e36e6f18eee580f
Bianca Carvalho <biancadccontato@gmail.com>
2026-06-13 21:58:45 -0300

    edição html da página de contato

--- js/segurança.js
+++ js/segurança.js
@@ -15,18 +15,15 @@ function gerarSenha() {
 
     let senha = "";
 
-    // Garante pelo menos um de cada tipo
     senha += letrasMinusculas[Math.floor(Math.random() * letrasMinusculas.length)];
     senha += letrasMaiusculas[Math.floor(Math.random() * letrasMaiusculas.length)];
     senha += numeros[Math.floor(Math.random() * numeros.length)];
     senha += especiais[Math.floor(Math.random() * especiais.length)];
 
-    // Preenche até o tamanho escolhido
     for (let i = 4; i < tamanho; i++) {
         senha += todos[Math.floor(Math.random() * todos.length)];
     }
 
-    // Embaralha
     senha = senha.split("").sort(() => Math.random() - 0.5).join("");
 
     document.getElementById("senha-gerada").textContent = senha;
